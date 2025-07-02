a8a23352561228e3191b25e60db91651333efe3d
KuroXSub <qurrotaayunliulinuha@gmail.com>
2025-07-02 21:43:11 +0700

    route efficiency

--- routes/web.php
+++ routes/web.php
@@ -7,38 +7,32 @@ use App\Livewire\Settings\Appearance;
 use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+
+
 Route::view('/about', 'about')->name('about');
 
-Route::get('auth/google', [GoogleController::class, 'redirectToGoogle']);
-Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+Route::prefix('auth/google')->controller(GoogleController::class)->group(function () {
+    Route::get('/', 'redirectToGoogle')->name('auth.google');
+    Route::get('callback', 'handleGoogleCallback');
+});
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', DashboardController::class)->name('dashboard');
-
-    Route::redirect('settings', 'settings/profile');
-
-    Route::get('settings/profile', Profile::class)->name('settings.profile');
-    Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
+    Route::get('dashboard', DashboardController::class)->name('dashboard');
 });
 
 Route::middleware(['auth', 'verified'])->prefix('dashboard')->group(function () {
     Route::resource('bookings', BookingController::class)->except(['show']);
 
-    Route::resource('bookings', BookingController::class)->names([
-        'index' => 'bookings.index',
-        'create' => 'bookings.create',
-        'store' => 'bookings.store',
-        'edit' => 'bookings.edit',
-        'update' => 'bookings.update',
-        'destroy' => 'bookings.destroy',
-    ]);
-
-    Route::get('/booking-availability', [BookingController::class, 'checkAvailability'])->name('booking.availability');;
+    Route::get('booking-availability', [BookingController::class, 'checkAvailability'])
+        ->name('booking.availability');
 });
+
+
+
+
+Route::get('/', [WelcomeController::class, 'index'])->name('home');
+
 
 require __DIR__.'/auth.php';
