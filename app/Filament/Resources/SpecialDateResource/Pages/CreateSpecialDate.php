58823168fb532ccf394d32202f501f6659984fd1
KuroXSub <qurrotaayunliulinuha@gmail.com>
2025-08-14 13:17:24 +0700

    Add route to index

--- app/Filament/Resources/SpecialDateResource/Pages/CreateSpecialDate.php
+++ app/Filament/Resources/SpecialDateResource/Pages/CreateSpecialDate.php
@@ -9,4 +9,9 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateSpecialDate extends CreateRecord
 {
     protected static string $resource = SpecialDateResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
